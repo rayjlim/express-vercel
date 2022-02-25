4865a981ae8e544604d9818b524adb2ad913bc14
ray <rayjlim1@gmail.com>
2022-02-25 15:44:12 -0800

    feat: 🚀 get quotes from file

--- api/product.js
+++ api/product.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pkg = require("../package.json")
+const quotes = require("./quotes.json")
 
 /**
  * GET product list.
@@ -9,9 +10,14 @@ const pkg = require("../package.json")
  */
 router.get("/", async (req, res) => {
   try {
+    const items = quotes.quotes;
+    const item = items[Math.floor(Math.random()*items.length)];
+
+
     res.json({
       header: pkg.version + ":"+ new Date(),
-      message: ": A random inspirational quote: "
+      message: item.quote,
+      author: item.author
     });
   } catch (error) {
     console.error(error);
